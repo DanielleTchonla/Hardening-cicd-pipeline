030a8259aaacfec889a0fcbde9d3a322d2c7241f
Danielle Tchonla <d.tchonla@gmail.com>
2025-08-30 20:30:02 -0700

    initial commit

---
+++ terraform/iam-github-oidc.tf
@@ -0,0 +1,64 @@
+
+# GitHub OIDC Provider
+# Allows AWS to trust GitHub Actions workflows
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+
+# IAM Role for GitHub Actions
+# This role will be assumed by GitHub Actions workflow via OIDC
+
+resource "aws_iam_role" "github_actions" {
+  name = "GitHubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::746669235620:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            # GitHub JWT audience must match
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            # Must match this repository and branch exactly
+            "token.actions.githubusercontent.com:sub" = "repo:DanielleTchonla/Hardening-cicd-pipeline:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+# IAM Role Policy Attachments
+# Attach AWS managed policies for ECR, EKS, CloudWatch
+
+resource "aws_iam_role_policy_attachment" "eks_cluster" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_poweruser" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+}
